6ad41739443af439b39c11b76c37809b605db941
MurtazaG786 <murtazagangardiwala8@gmail.com>
2025-12-30 00:45:17 +0530

    here goes the env

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
-const express=require("express")
+const dotenv=require("dotenv");
+const cors=require("cors");
+dotenv.config();
+const express=require("express");
 app=express()
+app.use(cors());
+app.use(express.json());
+
 app.get("/",(req,res)=>{
   res.send("server is working")
-})
-const port=5000
+});
+port=process.env.PORT || 5000 ;
 app.listen(port,()=>{
   console.log("listning on port..."+port)
 })
