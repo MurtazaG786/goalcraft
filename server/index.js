a0b25359ca985c07b1b315af097f0f6aaa0cc801
Murtaza Gangardiwala <murtazagangardiwala8@gmail.com>
2026-01-18 23:13:28 +0530

    Add root route to log server status
    Added a root route that logs a message when accessed.

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const express=require("express");
 app=express()
 app.use(cors());
 app.use(express.json());
-
+app.get("/",()=>{
+  console.log("server is running")
+})
 
 port=process.env.PORT || 5000 ;
 app.listen(port,()=>{
