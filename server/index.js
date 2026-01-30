95296adddd149a62558ce0a9f8fba29681277f7e
Murtaza Gangardiwala <murtazagangardiwala8@gmail.com>
2026-01-30 23:46:16 +0530

    Change log message from 'well' to 'good'

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ app=express()
 app.use(cors());
 app.use(express.json());
 app.get("/",()=>{
-  console.log("server is working well")
+  console.log("server is working good")
 })
 
 port=process.env.PORT || 5000 ;
